bc6c2e4ba381cee8de4e4bfe4ba139e4cc8dc044
andrew-L-au <99318727+andrew-L-au@users.noreply.github.com>
2022-03-09 15:05:26 +0800

    Update draw2.cpp

--- draw2.cpp
+++ draw2.cpp
@@ -10,71 +10,63 @@
 using namespace std;
 
 template<typename T>
-class Draw{
-    private:
+class Draw {
+private:
     vector<vector<T> > map;
+ 
     
-    template<T* = nullptr>
-        requires is_same<T, char>::value
-    void draw(int x,int y) {
+    void draw(int x, int y) requires is_same_v<T, char> {
         map[19 - y][x] = '1';
     }
-    
-    template<enable_if_t<is_same<T,bool>::value>* = nullptr>
-    void draw(int x,int y){
-        map[19 - y][x] = 0;
+
+    void draw(int x, int y) requires is_same_v<T, bool> {
+        map[19 - y][x] = false;
     }
-    
-    public:
 
-    template<enable_if_t<is_same<T,char>::value>* = nullptr>
-    Draw():map(20,vector<char>(20,'0')){};
+public:
 
-    template<enable_if_t<is_same<T,bool>::value>* = nullptr>
-    Draw():map(20,vector<bool>(20,false)){};
+    template<typename T1 = T> requires is_same_v<T, char>
+    Draw() :map(20, vector<char>(20, '0')) {};
 
-    void printResult(){
-        for(auto row:map)
+    template<typename T1 = T> requires is_same_v<T, bool>
+    Draw() :map(20, vector<bool>(20, false)) {};
+
+    void printResult() {
+        for (auto row : map)
         {
-            for(auto column : row)
+            for (auto column : row)
             {
-                cout<<column<<' ';
+                cout << column << ' ';
             }
-            cout<<endl;
+            cout << endl;
         }
-        cout<<endl<<endl;
+        cout << endl << endl;
     }
 
-    void line(int x0,int y0,int x1,int y1){
+    void line(int x0, int y0, int x1, int y1) {
         int flag = 0;
-        if(y1 - y0>x1 - x0||y1 - y0<x0 - x1)
+        if (y1 - y0 > x1 - x0 || y1 - y0 < x0 - x1)
         {
-            swap(x0,y0);
-            swap(x1,y1);
+            swap(x0, y0);
+            swap(x1, y1);
             flag = 1;
         }
-        if(x0>x1)
+        if (x0 > x1)
         {
-            swap(x0,x1);
-            swap(y0,y1);
+            swap(x0, x1);
+            swap(y0, y1);
         }
-        double t = (double)(y1 - y0)/(x1 - x0);
-        for(int x = x0;x<=x1;++x)
+        double t = (double)(y1 - y0) / (x1 - x0);
+        for (int x = x0; x <= x1; ++x)
         {
-            int y = round(y0 + t*(x-x0));
-            if(flag)draw(y,x);
-            else draw(x,y);
+            int y = round(y0 + t * (x - x0));
+            if (flag)draw(y, x);
+            else draw(x, y);
         }
     }
 };
 
-int main(){
+int main() {
     Draw<bool> draw;
-    draw.printResult();
-    //2. 绘制第⼀条线后printResult结果
-    draw.line(2, 0, 2, 19);
-    draw.printResult();
-    //3. 绘制第⼆条线后printResult结果
-    draw.line(0, 2, 19, 2);
-    draw.printResult();
+
 }
